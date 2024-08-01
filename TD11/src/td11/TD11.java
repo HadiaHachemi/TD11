98ad422f36898d1d897a8e49902b2e1368221743
Hadia Hachemi <73354929+HadiaHachemi@users.noreply.github.com>
2024-08-01 18:44:19 -0400

    Update TD11.java

--- TD11/src/td11/TD11.java
+++ TD11/src/td11/TD11.java
@@ -15,6 +15,7 @@ public class TD11 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour "+args[1]+", " +args[0]); 
+        System.out.println("Au revoir!");
     }
     
 }
