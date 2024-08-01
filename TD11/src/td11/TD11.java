a90ccbb3edae440796764590b32e736a27e881fc
HadiaHachemi <hadiahm20@gmail.com>
2024-08-01 15:34:33 -0400

    Test 3

---
+++ TD11/src/td11/TD11.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package td11;
+
+/**
+ *
+ * @author Hadia Hachemi
+ */
+public class TD11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour "+args[1]+", " +args[0]); 
+    }
+    
+}
